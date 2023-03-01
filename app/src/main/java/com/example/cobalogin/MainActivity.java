e99362032704f018dac8c228bdb5d969c3d6679d
oriell754 <fernand0753@mhs.mdp.ac.id>
2023-03-01 09:23:29 +0700

    update

--- app/src/main/java/com/example/cobalogin/MainActivity.java
+++ app/src/main/java/com/example/cobalogin/MainActivity.java
@@ -4,31 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvsesi;
+    private TextView tvsesi,tvnama;
     private Button Btnlogout;
 
     kendali_login kl=new kendali_login();
     public  static String keySpusername="sppadang";
+    public  static String keySpname="spadang";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         if(kl.islogin(MainActivity.this,keySpusername)==true){
+            setContentView(R.layout.activity_main);
+            tvnama=findViewById(R.id.tv_nama);
             tvsesi = findViewById(R.id.tv_sesi);
             Btnlogout =findViewById(R.id.btn_logout);
 
+            tvsesi.setText(kl.getpref(MainActivity.this,keySpusername));
+            tvnama.setText(kl.getpref(MainActivity.this,keySpname));
+
+            Btnlogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    kl.setpref(MainActivity.this,keySpusername, null);
+                    kl.setpref(MainActivity.this,keySpname, null);
+                    startActivity(new Intent(MainActivity.this, login_activity.class));
+                    finish();
+                }
+            });
         }
         else{
             startActivity(new Intent(MainActivity.this, login_activity.class));
             finish();
         }
 
-//        setContentView(R.layout.activity_main);
+
 
 
 
--- app/src/main/java/com/example/cobalogin/login_activity.java
+++ app/src/main/java/com/example/cobalogin/login_activity.java
@@ -2,13 +2,56 @@ package com.example.cobalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login_activity extends AppCompatActivity {
+    private EditText etusername,etpassword;
+    private Button btnlogin;
+
+    private String username,password;
+
+    kendali_login KL=new kendali_login();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_login);
+
+        etusername=findViewById(R.id.et_username);
+        etpassword=findViewById(R.id.et_password);
+        btnlogin=findViewById(R.id.btn_login);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username=etusername.getText().toString();
+                password=etpassword.getText().toString();
+
+                if(username.trim().equals("")){
+                    etusername.setError("USERNAME TIDAK BOLEH KOSONG!!!!!");
+                }
+                else if(password.trim().equals("")){
+                    etpassword.setError("PASSWORD TIDAK BOLEH KOSONG!!!!");
+                }
+                else {
+                        if (username.equals("nando")&&password.equals("123")){
+                            KL.setpref(login_activity.this,MainActivity.keySpusername,username);
+                            KL.setpref(login_activity.this,MainActivity.keySpname,"Doooo");
+                            startActivity(new Intent(login_activity.this,MainActivity.class));
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(login_activity.this,"USERNAME ATAU PASSWORD ADA YANG SALAH",Toast.LENGTH_LONG).show();
+                        }
+                }
+            }
+        });
+
     }
 }
